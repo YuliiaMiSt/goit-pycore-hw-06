48747e6521e76de26d768ca1bec0e467379341e5
YuliiaMiSt <Reju1910@ukr.net>
2024-07-26 20:15:03 +0200

    Task_6

---
+++ Homework_5.py
@@ -0,0 +1,61 @@
+#Task_1#
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+        super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        super().__init__(value)
+        self.validate(value)
+
+    def validate(self, value):
+        if not value.isdigit() or len(value) != 10:
+            raise ValueError("Invalid phone number format")
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def remove_phone(self, phone):
+        self.phones = [p for p in self.phones if p.value != phone]
+
+    def edit_phone(self, old_phone, new_phone):
+        for i, p in enumerate(self.phones):
+            if p.value == old_phone:
+                self.phones[i] = Phone(new_phone)
+                break
+
+    def find_phone(self, phone):
+        return any(p.value == phone for p in self.phones)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+
+    def find_records(self, name):
+        return [record for record in self.data.values() if record.name.value == name]
+
+    def remove_records(self, name):
+        for record in self.find_records(name):
+            del self.data[record.name.value]
+#Task_1
+
+#Task_2#
+
+
